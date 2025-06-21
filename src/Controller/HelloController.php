ccad094ab84b0bacaaff65e1c2c70cd166c58373
rafalander <rafa.lander@hotmail.com>
2025-06-21 14:01:01 -0300

    feature(INIT): Primeiro commit do projeto

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+       return new Response (content:'Ola mundo!');
+    }
+    
+    #[Route('/dinossauro')]
+    public function dinossauro(): Response
+    {
+       return new Response (content:'Ola dinossauro!');
+    }
+
+    #[Route('/mamifero')]
+    public function mamifero(): Response
+    {
+       return new Response (content:'Ola mamifero!');
+    }
+    
+}
+
