ecb0f137cd8fd5c82d81b0afc5a8f00359098137
rafalander <rafa.lander@hotmail.com>
2025-06-22 13:35:13 -0300

    feature(): Ajustes no hello controller, criacao de variaveis para melhor funcionamento do codigo

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
-class HelloController
+
+class HelloController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-       return new Response (content: 'Bem vindo ao nosso Zoológico');
+        $title = 'Zoo Lander';
+        $navbarTitle = '<strong style="color:darkgreen;">Zoo Lander</strong>';
+        $welcome = 'Bem vindo ao nosso zoológico';
+
+        return $this->render('hello/homepage.html.twig', [
+          'title'       => $title,
+          'navbarTitle' => $navbarTitle,
+          'welcome'     => $welcome . ' ' . $navbarTitle,
+        ]);
     }
-    
+
     #[Route('/animal/{slug}')]
-    public function animal(string $slug=null): Response
+    public function animal(string $slug = null): Response
     {
-      $newSlug = str_replace('-', ' ', $slug);
-      $newSlug = u($newSlug)->title(true)->toString();
-      return new Response (content:'Olá ' . $newSlug);
+        $newSlug = str_replace('-', ' ', $slug);
+        $newSlug = u($newSlug)->title(true)->toString();
+
+        return new Response('Olá ' . $newSlug);
     }
 }
